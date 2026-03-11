3514668119af980905f7b5518bcb789cc799d9be
Yaroslav Hut <s32811@pjwstk.edu.pl>
2026-03-11 20:10:09 +0100

    Added personalized output line

--- apbd_t1/Program.cs
+++ apbd_t1/Program.cs
@@ -3,3 +3,10 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello there!");
+
+Console.WriteLine("Enter username:");
+
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + userName + " now we are friends!");
